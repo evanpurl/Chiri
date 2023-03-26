02db5a325820e77c550dc191047fbaffe1d3fca0
evanpurl <evan@purlcomputers.com>
2023-03-25 23:50:49 -0600

    Logging test

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from discord.ext import commands, tasks
 from database.database import gettoken
 from database.testdbconnection import connect
 from util.load_extensions import load_extensions
+import logging
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
 client = commands.Bot(command_prefix="$", intents=intents)
+
+handler = logging.FileHandler(filename='chiri.log', encoding='utf-8', mode='w')
 
 
 # Main function to load extensions and then load bot.
@@ -19,7 +22,7 @@ async def main():
             token = await gettoken("chiri")
             await connect()
             await load_extensions(client)
-            await client.start(token[0])
+            await client.run(token[0], log_handler=handler, log_level=logging.DEBUG)
         except KeyboardInterrupt:
             pass
 
