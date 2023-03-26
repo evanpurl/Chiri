d575c4b4be9bc957fc36b697ad0ca6eb4141f352
evanpurl <evan@purlcomputers.com>
2023-03-25 23:56:15 -0600

    Logging test

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import asyncio
 import discord
-from discord.ext import commands, tasks
+from discord.ext import commands
 from database.database import gettoken
 from database.testdbconnection import connect
 from util.load_extensions import load_extensions
 import logging
+import logging.handlers
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,7 +13,21 @@ intents.members = True
 
 client = commands.Bot(command_prefix="$", intents=intents)
 
-handler = logging.FileHandler(filename='chiri.log', encoding='utf-8', mode='w')
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+logging.getLogger('discord.http').setLevel(logging.INFO)
+
+handler = logging.handlers.RotatingFileHandler(
+    filename='chiri.log',
+    encoding='utf-8',
+    maxBytes=32 * 1024 * 1024,  # 32 MiB
+    backupCount=5,  # Rotate through 5 files
+)
+dt_fmt = '%Y-%m-%d %H:%M:%S'
+formatter = logging.Formatter('[{asctime}] [{levelname:<8}] {name}: {message}', dt_fmt, style='{')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 
 # Main function to load extensions and then load bot.
@@ -22,7 +37,7 @@ async def main():
             token = await gettoken("chiri")
             await connect()
             await load_extensions(client)
-            await client.run(token[0], log_handler=handler, log_level=logging.DEBUG)
+            await client.run(token[0])
         except KeyboardInterrupt:
             pass
 
